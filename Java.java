e94efc7821db50ffa89808a33fe4a5be84af5896
shrushti kavatekar <shrushtikavatekar7@gmail.com>
2026-03-02 22:53:48 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,9 @@
+class Java {
+
+public static void main(String[] args){
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println("Is java fun? " + isJavaFun);
+System.out.println("Is fish Tasty?" +isFishTasty);
+}
+}
