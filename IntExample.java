a5d53691a700963ca0c4f69b48593c72e77a1cb6
shrushti kavatekar <shrushtikavatekar7@gmail.com>
2026-03-04 23:14:25 +0530

    Add files via upload

---
+++ IntExample.java
@@ -0,0 +1,10 @@
+class IntExample{
+ public static void main(String[] args){
+ 
+ int age = 21;
+ int marks = 85;
+ 
+ System.out.println("Age: " + age);
+ System.out.println("Marks: " +marks);
+ }
+ }
