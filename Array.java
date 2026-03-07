254bf6cc52f9e2443815d40577ab3b276d37126e
shrushti kavatekar <shrushtikavatekar7@gmail.com>
2026-03-07 23:10:03 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+class Array {
+
+public static void main (String[]args){
+
+int arr[]= {100,123,34};
+
+  for (int num : arr) {
+  
+  System.out.println(num);
+  
+  }
+  
+  }
+  }
---
+++ Websites.java
@@ -0,0 +1,18 @@
+class Websites {
+ static String name  = "shrushti";
+public static  void main (String[]web){
+     myName();
+}
+
+public static void myName(){
+System.out.println ("My name is :" + name); 
+}
+
+
+
+
+
+
+
+
+}
